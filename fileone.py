f849b4a84db1a96c04cb66eaeb27987bac952eb7
Aktan Ulanbekov <aktan.ulanbekov@gmail.com>
2023-04-02 19:01:11 +0300

    можно написать что хочешь

---
+++ fileone.py
@@ -0,0 +1,13 @@
+'''
+
+git config --global username "Aktan Ulanbekov"
+git config --global useremail "aktan.ulanbekov@gmail.com"
+git init
+git status
+git add fileone.py
+git add filetwo.py
+git commit -m "можно написать что хочешь"
+git remote add origin gitссылка  
+git push origin main
+
+'''
